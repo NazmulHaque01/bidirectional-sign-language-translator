1aeebd5b79cbd221fab5a8f0614b70e290c77649
Naiem Racer <mdnazmulhaque0033@gmail.com>
2026-05-06 05:44:14 +0600

    Feat: Configure Progressive Web App (PWA) with manifest, service worker, and app icon

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sign-translator-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/config.js',
+  './js/speech-to-sign.js',
+  './js/sign-to-speech.js',
+  './js/utils.js',
+  './icon-512.png'
+];
+
+// Install Event - Cache Core Assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching app shell');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event - Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event - Serve from Cache, Fallback to Network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
